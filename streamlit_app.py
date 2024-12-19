346ab6a7b1da132f3ffb4df19183d49aeda8e78f
Philipp Schrittwieser <123021085+Philipp-Schrittwieser@users.noreply.github.com>
2024-12-19 10:26:28 +0000

    Lebenslauf fast fertig

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,37 @@
 import streamlit as st
+from pathlib import Path
+
+st.markdown("<style> .stAppHeader {display:none;} ul {list-style-type: none; } </style>", unsafe_allow_html=True)
+
+def get_file_content_as_bytes(file_path):
+    with open(file_path, "rb") as file:
+        return file.read()
+
+# Pfad zur PDF-Datei
+file_path = 'test.pdf'
+
+# Lese den Inhalt der PDF-Datei als Bytes
+file_bytes = get_file_content_as_bytes(file_path)
 
 left, right = st.columns(2)
 
-left.image("picture.jpg", width=250)
+left.image("profile-pic.png", width=250)
 
-right.header("Vorname Nachname")
+with right:
+    st.markdown("""
+                <h3>Philipp Schrittwieser</h3>
+                <em>Ich bin davon überzeugt, dass KI die Welt verändern wird
+                und ich möchte ein Teil davon sein.</em>
+                """, unsafe_allow_html=True)
+
+    st.download_button(
+            label="📄 Download Lebenslauf",
+            data=file_bytes,
+            file_name=file_path,
+            mime='application/pdf'
+    )
+
+    st.write("📩", "philipp.schrittwieser@edu.magwien.gv.at")
 
 st.header("IT-Kompetenzen", anchor=False, divider="blue")
 
